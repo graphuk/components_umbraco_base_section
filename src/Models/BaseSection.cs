e3ca88775a8f019f3a6eb09bb2d6b9c1ea482697
ilya.skrobot <ilya.skrobot@graph.uk>
2019-02-11 14:51:15 +0500

    use UmbracoGridConfigLoader

--- src/Models/BaseSection.cs
+++ src/Models/BaseSection.cs
@@ -1,84 +1,24 @@
-using System.Collections.Generic;
-using Graph.Components.ContentBlocks;
+using UmbracoGridConfigLoader.Attributes;
+using UmbracoGridConfigLoader.Models;
 
 namespace Graph.Components.BaseSection
 {
-	public class BaseSection : GridSettingsLoader.IGridConfigLoader
+	public class BaseSection : IGridConfigLoader
 	{
-		private static readonly Dictionary<string, string> ApplyTo = new Dictionary<string, string>
-		{
-			{"row", nameof(BaseSection)}
-		};
+		[GridConfigProperty(Label = "Title", View = PreValuesEditors.TextString)]
+		public IGridConfig Title { get; set; }
 
-		private static string GetKey(string key) => $"{key}{nameof(BaseSection)}";
+		[GridConfigProperty(Label = "Summary", View = PreValuesEditors.Textarea)]
+		public IGridConfig Summary { get; set; }
 
-		public GridSettingsLoader.Config[] Config =
-		{
-			new GridSettingsLoader.Config
-			{
-				Label = "Title",
-				Description = "",
-				Key = GetKey("title"),
-				View = "textstring",
-				ApplyTo = ApplyTo
-			},
-			new GridSettingsLoader.Config
-			{
-				Label = "Summary",
-				Description = "",
-				Key = GetKey("summary"),
-				View = "textarea",
-				ApplyTo = ApplyTo
-			},
-			new GridSettingsLoader.Config
-			{
-				Label = "CTA Button Link",
-				Description = "",
-				Key = GetKey("ctaButton"),
-				View = "/App_Plugins/GibeLinkPicker/picker.html",
-				Modifier = "json",
-				ApplyTo = ApplyTo
-			},
-			new GridSettingsLoader.Config
-			{
-				Label = "Theme",
-				Description = "",
-				Key = GetKey("theme"),
-				View = "radiobuttonlist",
-				ApplyTo = ApplyTo,
-				Prevalues = new []
-				{
-					new GridSettingsLoader.Prevalue
-					{
-						Label = "Primary",
-						Value = "primary"
-					},
-					new GridSettingsLoader.Prevalue
-					{
-						Label = "Secondary",
-						Value = "secondary"
-					}
-				}
-			}
-		};
+		[GridConfigProperty(Label = "CTA Button Link", View = "/App_Plugins/GibeLinkPicker/picker.html", Modifier = "json")]
+		public IGridConfig CtaButton { get; set; }
 
-		public GridSettingsLoader.Layout[] Layout =
-		{
-			new GridSettingsLoader.Layout
-			{
-				Label = "Base Section",
-				Name = nameof(BaseSection),
-				Areas = new []
-				{
-					new GridSettingsLoader.Area
-					{
-						Grid = 12,
-						Editors = new List<string>(),
-						AllowAll = true,
-						MaxItems = 0
-					}
-				}
-			}
-		};
+		[GridConfigProperty(Label = "Theme", View = PreValuesEditors.Radiobuttonlist, Prevalues = new[] {"Primary", "Secondary"})]
+		public IGridConfig Theme { get; set; }
+
+		[GridLayoutProperty(Label = "Base Section")]
+		public IGridLayout Layout { get; set; }
+
 	}
 }
